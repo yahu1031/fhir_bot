139ab34256e1997a2567a99098efe378f88d1af3
yahu1031 <121003219@sastra.ac.in>
2021-07-08 20:22:15 +0530

    version updated

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.tickets_category = process.env.TICKETS_CATEGORY;
 client.hackthon_rules_channel = process.env.HACKATHON_RULES_CHANNEL;
 client.hacker_role_id = process.env.HACKER_ROLE_ID;
 client.at_support_channel = process.env.AT_SUPPORT_CHANNEL;
-client.version = '0.0.1-alpha';
+client.version = '1.0.3';
 
 const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
