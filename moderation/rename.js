2465ee13296c27e85309e656b57ab52661c1cb5b
yahu1031 <121003219@sastra.ac.in>
2021-07-24 00:53:36 +0530

    fixed rename command 🛠️

--- moderation/rename.js
+++ moderation/rename.js
@@ -4,25 +4,23 @@ module.exports = {
     name: 'rename',
     description: 'This command will rename the user nicknames.',
     args: true,
-    async execute(client, message = new Message()) {
+    async execute(client, message = new Message(), args) {
         if (message.author.bot) return;
         if (!message.member.permissions.has('ADMINISTRATOR')) return;
         const guildDATA = await client.guilds.fetch(client.guildID);
-        // console.log(userRole);
         const mem = await guildDATA.members.fetch();
         const chan = await message.mentions.channels.first();
         const userRole = await message.mentions.roles.first();
-        console.log(userRole.id);
         mem.forEach(async (member = new GuildMember()) => {
             try {
                 if (!member.user.bot && guildDATA.ownerId !== member.user.id) {
-                    if (member.roles.cache.has(userRole.id)) {
+                    if (!chan && !userRole) {
+                        await member.setNickname(null);
+                    }
+                    else if (userRole && member.roles.cache.has(userRole.id)) {
                         await member.setNickname(null);
                     }
                     else if (chan && member.permissionsIn(chan).has('VIEW_CHANNEL')) {
-                        await member.setNickname(null);
-                    }
-                    else if (!chan) {
                         await member.setNickname(null);
                     }
                 }
