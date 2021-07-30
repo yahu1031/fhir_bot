6d21ece21b03c20f1a7af031a70f7e25ff40896b
yahu1031 <121003219@sastra.ac.in>
2021-07-31 01:35:04 +0530

    updated rename command ✏️

--- moderation/rename.js
+++ moderation/rename.js
@@ -9,26 +9,60 @@ module.exports = {
         if (!message.member.permissions.has('ADMINISTRATOR')) return;
         const guildDATA = await client.guilds.fetch(client.guildID);
         const mem = await guildDATA.members.fetch();
-        const chan = await message.mentions.channels.first();
-        const userRole = await message.mentions.roles.first();
-        mem.forEach(async (member = new GuildMember()) => {
-            try {
-                if (!member.user.bot && guildDATA.ownerId !== member.user.id) {
-                    if (!chan && !userRole) {
-                        await member.setNickname(null);
+        const chan = message.mentions.channels.first();
+        const userRole = message.mentions.roles.first();
+        if (args[1] === 'reset'.toLowerCase()) {
+            if (chan || userRole || args[0] === 'all'.toLowerCase()) {
+                await mem.forEach(async (member = new GuildMember()) => {
+                    try {
+                        if (!member.user.bot && guildDATA.ownerId !== member.user.id) {
+                            if (!chan && !userRole && args[0] === 'all'.toLowerCase()) {
+                                await member.setNickname(null);
+                            }
+                            else if (userRole && member.roles.cache.has(userRole.id)) {
+                                await member.setNickname(null);
+                            }
+                            else if (chan && member.permissionsIn(chan).has('VIEW_CHANNEL')) {
+                                await member.setNickname(null);
+                            }
+                        }
                     }
-                    else if (userRole && member.roles.cache.has(userRole.id)) {
-                        await member.setNickname(null);
+                    catch (err) {
+                        return console.error(`💥 ${member.nickname} or ${member.user.username} has error resetting nickname.`);
                     }
-                    else if (chan && member.permissionsIn(chan).has('VIEW_CHANNEL')) {
-                        await member.setNickname(null);
+                });
+                return message.channel.send('Resetting nicknames was done');
+            }
+        }
+        else if (args[1] === 'add'.toLowerCase()) {
+            const unchangeUser = [];
+            if (userRole) {
+                mem.forEach(async (member = new GuildMember()) => {
+                    try {
+                        if (member.roles.cache.has(userRole.id)) {
+                            if (member.nickname === null) {
+                                await member.setNickname(`${member.user.username} ${args[2]}`);
+                            }
+                            else {
+                                if (member.nickname.includes(args[2])) {
+                                    unchangeUser.push(member.user.username);
+                                }
+                                else {
+                                    await member.setNickname(`${member.nickname} ${args[2]}`);
+                                }
+                            }
+                        }
                     }
+                    catch (err) {
+                        return console.error(`💥 ${member.nickname} or ${member.user.username} has error setting nickname.`);
+                    }
+                });
+                if (unchangeUser.length > 0) {
+                    return message.channel.send(`${unchangeUser.toString().replace(',', ', ')} already has nicknames including ${args[2]}`);
                 }
+                return message.channel.send('Setting nicknames was done');
             }
-            catch (err) {
-                console.error(`${member.nickname} or ${member.user.username} has error`);
-            }
-        });
-        return message.channel.send('Nickname set to null');
+        }
+        return message.channel.send('Check the command please.\n**USAGE:** !rename <@Role>|<#Channel>|all reset|add DATA\n**EG: !rename @role add 🔥\n or\n !rename @role \\|\\| #channel \\|\\| all reset**');
     },
 };
